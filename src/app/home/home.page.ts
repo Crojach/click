596b15a96a39c13c6fdc142f145ee8ea8cada355
Ivan Karacic <ivan.crojach.karacic@gmail.com>
2020-12-17 12:51:32 +0100

    Added buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  source: string;
   constructor() {}
+  
+  ngOnInit(): void {
+    document
+      .getElementById('button')
+      .addEventListener('click', (event: Event) => {
+        this.setSource(event.isTrusted)
+      })
+  }
 
+  onRobotClick() {
+    document.getElementById('button').click();
+  }
+
+  setSource(isTrusted: boolean) {
+    this.source = isTrusted ? 'User' : 'Robot'
+  }
 }
